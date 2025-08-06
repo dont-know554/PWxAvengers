5ec671ade8c78a23915bbde8f9fd7bd407cdc5e8
Zayed <toptricks8614@gmail.com>
2025-08-06 17:36:57 +0530

    avengers

--- netlify/functions/batch-content.js
+++ netlify/functions/batch-content.js
@@ -18,45 +18,11 @@ exports.handler = async (event, context) => {
   }
 
   try {
-    // Log the incoming request for debugging
-    console.log('Incoming request path:', event.path);
-    console.log('HTTP Method:', event.httpMethod);
-    console.log('Raw URL:', event.rawUrl);
-    console.log('Headers:', event.headers);
-    
-    // Extract parameters from the original URL using regex
-    // The original URL is available in event.rawUrl or we can parse from headers
-    const originalUrl = event.rawUrl || event.headers['x-forwarded-for'] || event.path;
-    console.log('Original URL:', originalUrl);
-    
-    // Parse the URL to extract parameters
-    let batchId, subjectSlug, scheduleId;
-    
-    // Try to match the pattern: /api/batch/{batchId}/subject/{subjectSlug}/schedule/{scheduleId}/content
-    const urlMatch = originalUrl.match(/\/api\/batch\/([^/]+)\/subject\/([^/]+)\/schedule\/([^/]+)\/content/);
-    
-    if (urlMatch) {
-      batchId = urlMatch[1];
-      subjectSlug = urlMatch[2];
-      scheduleId = urlMatch[3];
-    } else {
-      // Fallback: try to extract from path segments if redirect didn't work as expected
-      const pathSegments = event.path.split('/');
-      console.log('Path segments:', pathSegments);
-      
-      // Look for the pattern in path segments
-      const batchIndex = pathSegments.indexOf('batch');
-      const subjectIndex = pathSegments.indexOf('subject');
-      const scheduleIndex = pathSegments.indexOf('schedule');
-      
-      if (batchIndex !== -1 && subjectIndex !== -1 && scheduleIndex !== -1) {
-        batchId = pathSegments[batchIndex + 1];
-        subjectSlug = pathSegments[subjectIndex + 1];
-        scheduleId = pathSegments[scheduleIndex + 1];
-      }
-    }
-    
-    console.log('Extracted parameters:', { batchId, subjectSlug, scheduleId });
+    // Extract parameters from the path
+    const pathSegments = event.path.split('/');
+    const batchId = pathSegments[4];
+    const subjectSlug = pathSegments[5];
+    const scheduleId = pathSegments[6];
     
     const apiUrl = `https://pw-api1-ab3091004643.herokuapp.com/api/batch/${batchId}/subject/${subjectSlug}/schedule/${scheduleId}/content`;
     
